898a14790dc5994ded2580d31fd4dc4916e849f7
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-18 04:33:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :breed
   def initialize(name, breed)
     @name = name
-    if breed == nil
+    if @breed == nil
       @breed = "Mutt"
     else
       @breed = breed
